e8bf7ad74162d0dc0c14d6bbd2995c3e00df0d44
N-Tarin <karinuxxx1029@gmail.com>
2021-02-23 13:16:12 +0900

    changed

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@ function my_script_init() {
   wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
 
   //JSの読み込み 
-  // wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', array(), '5.8.2', 'all');
   wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
